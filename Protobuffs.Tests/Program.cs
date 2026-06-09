cbda5f5a59b761fa6cff2210eb026e150fde7a1f
Diego Balderrama <diegowrhasta@gmail.com>
2026-06-10 07:21:15 +0900

    Adds initial commit for simple proto generation and usage

---
+++ Protobuffs.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProtoDemo;
+
+var person = new Person
+{
+    Id = 1001,
+    FirstName = "Diego",
+    LastName = "Balderrama",
+    Active = true,
+    Salary = 120000,
+    Address = new Address
+    {
+        Street = "123 Main St",
+        City = "Tokyo"
+    }
+};
+
+person.Skills.Add("C#");
+person.Skills.Add(".NET");
+person.Skills.Add("Docker");
+
+Console.WriteLine(person);
+
